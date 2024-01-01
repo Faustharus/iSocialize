82664042b1dd849c2128a4074fc44697e020c0bd
Faustharus <chailloleau-damien@outlook.fr>
2024-01-01 13:35:51 +0100

    Initial Commit

--- iSocialize/iSocializeApp.swift
+++ iSocialize/iSocializeApp.swift
@@ -5,13 +5,25 @@
 //  Created by Damien Chailloleau on 01/01/2024.
 //
 
+import FirebaseCore
 import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
 struct iSocializeApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationStack {
+                ContentView()
+            }
         }
     }
 }

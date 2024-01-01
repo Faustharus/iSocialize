a6a09ba90bc95d7456fbba9f349ea79ea55b1244
Faustharus <chailloleau-damien@outlook.fr>
2024-01-01 12:19:11 +0100

    Initial Commit

---
+++ iSocialize/iSocializeApp.swift
@@ -0,0 +1,17 @@
+//
+//  iSocializeApp.swift
+//  iSocialize
+//
+//  Created by Damien Chailloleau on 01/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct iSocializeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
